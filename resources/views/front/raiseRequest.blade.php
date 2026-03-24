d4896c183f57645f5a6a126577ae4a31266fdf21
vw-uday <verloop.dev4@gmail.com>
2026-03-24 17:37:45 +0530

    env file changes

--- resources/views/front/raiseRequest.blade.php
+++ resources/views/front/raiseRequest.blade.php
@@ -22,9 +22,9 @@
                     <div class="txt-block left-column">
                         <span class="section-id"></span>
                         <h2 class="w-700">Raise a <span class="color--green-500">Request</span></h2>
-                        <p class="p-md w-400">
+                        <!-- <p class="p-md w-400">
                             Have a query? Raise a request with EasyAILoans and get quick assistance for all your loan needs.
-                        </p>
+                        </p> -->
                     </div>
                 </div>
             </div>
