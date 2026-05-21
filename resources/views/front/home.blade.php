66f55f62e2b7cc709771d823136e695b28686d53
vw-uday <verloop.dev4@gmail.com>
2026-05-21 18:04:22 +0530

    home page changes

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -445,7 +445,8 @@
                 </div>
             </div>
         </div>
-        <div class="row p-30 bg--white-100 shadow border-grey-1 r-20">
+        <div class="p-30 bg--white-100 shadow border-grey-1 r-20">
+      <div class="row">
             <div class="col-md-7 order-first order-md-2">
                 <div id="emicalculatorinnerformwrapper">
                     <form id="emicalculatorform" class="comment-form">
@@ -683,6 +684,8 @@
                 </div>
             </div>
         </div>
+        </div>
+  
     </div>
 </section>
 <!-- eligibility calculator ends -->
